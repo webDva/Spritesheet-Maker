d1b591a4d4ea989226a6855883a96176fdaa3e3f
Web D.va <webNoDva@gmail.com>
2017-09-11 03:21:45 -0400

    file uploading

--- backend/main.js
+++ backend/main.js
@@ -3,31 +3,45 @@ let mysql = require('mysql');
 let multer = require('multer');
 let cors = require('cors');
 let fs = require('fs');
-let http = require('http');
 let path = require('path');
 
+// Setup
+const UPLOAD_PATH = './backend/uploads';
+const upload = multer({dest: `${UPLOAD_PATH}/`});
+
+/*
+ * API Server
+ */
+
 const app = express();
+app.use(cors()); // Needed for file sharing.
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+// File upload
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send(req.file);
+    console.log('receieved a file');
+});
+
+/*
+ * HTTP Server
+ */
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+// Start the HTTP server.
+app.listen(port, (errorThatOccurs) => {
+    if (errorThatOccurs) {
+        return console.log('Something bad happened.', errorThatOccurs);
+    }
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+    console.log(`Server running on localhost:${port}`);
+});
